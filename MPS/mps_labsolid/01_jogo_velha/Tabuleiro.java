fee9b88a15d10b636fea96aa38c17da691ef96a2
Igor Matheus <72845501+Igor-Matheus@users.noreply.github.com>
2022-11-10 16:51:33 -0300

    Update Tabuleiro.java

--- MPS/mps_labsolid/01_jogo_velha/Tabuleiro.java
+++ MPS/mps_labsolid/01_jogo_velha/Tabuleiro.java
@@ -1,6 +1,70 @@
 import java.util.ArrayList;
 
 
+public class Linhas {
+    ArrayList<String> spots;	
+
+    public ArrayList<String> primeiraLinha() {
+        ArrayList<String> firstRow = new ArrayList<String>();
+        firstRow.add(this.spots.get(0));
+        firstRow.add(this.spots.get(1));
+        firstRow.add(this.spots.get(2));
+        return firstRow;
+    }
+
+    public ArrayList<String> segundaLinha() {
+        ArrayList<String> secondRow = new ArrayList<String>();
+        secondRow.add(this.spots.get(3));
+        secondRow.add(this.spots.get(4));
+        secondRow.add(this.spots.get(5));
+        return secondRow;
+    }
+
+    public ArrayList<String> terceiraLinha() {
+        ArrayList<String> thirdRow = new ArrayList<String>();
+        thirdRow.add(this.spots.get(6));
+        thirdRow.add(this.spots.get(7));
+        thirdRow.add(this.spots.get(8));
+        return thirdRow;
+    }	
+}	
+
+public class Tabuleiro {
+    
+    public Tabuleiro() {
+        this.spots = new ArrayList<String>();
+        for (int i = 0; i < 9; i++) {
+            this.spots.add(" ");
+        }
+    }
+}
+
+public class Vitoria {
+     
+    public Boolean verificaGanhador(){
+    	if((this.spots.get(0) == this.spots.get(1)) && (this.spots.get(0) == this.spots.get(2))){
+    		return true;
+    	}else if ((this.spots.get(3) == this.spots.get(4)) && (this.spots.get(3) == this.spots.get(5))) {
+			return true;
+    	}else{
+    		return false;
+    	}
+    }   
+}
+    
+public class VisualizacaoTabuleiro {
+	
+    public void display() {
+        String tabuleiroFormatado = this.spots.get(0) + " | " + this.spots.get(1) + " | " + this.spots.get(2) + "\n" + this.spots.get(3) + " | " + this.spots.get(4) + " | " + this.spots.get(5) + "\n" + this.spots.get(6) + " | " + this.spots.get(7) + " | " + this.spots.get(8);
+        System.out.print(tabuleiroFormatado);
+    }
+}
+
+
+/*
+import java.util.ArrayList;
+
+
 
 public class Tabuleiro {
     ArrayList<String> spots;
@@ -52,4 +116,4 @@ public class Tabuleiro {
         String tabuleiroFormatado = this.spots.get(0) + " | " + this.spots.get(1) + " | " + this.spots.get(2) + "\n" + this.spots.get(3) + " | " + this.spots.get(4) + " | " + this.spots.get(5) + "\n" + this.spots.get(6) + " | " + this.spots.get(7) + " | " + this.spots.get(8);
         System.out.print(tabuleiroFormatado);
     }
-}
+} */
