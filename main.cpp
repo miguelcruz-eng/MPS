041ef66d82b23743c337883368c4d0d7d9688ee5
Felipe.dev <84815453+Felipe-Rod-Ass@users.noreply.github.com>
2022-12-01 11:53:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <conio.h>
 
 #include "userForm.h"
 
 int main(void)
 {
+    
+    FILE *arqUser
     userForm mn;
     mn.Menu();
     return 0;
